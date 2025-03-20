5998cc5254cebe40300519d554af5d10516c9511
bhubanesh-dev <bhubanesh.dev@gmail.com>
2025-03-20 18:27:04 +0530

    implemented search movie

--- src/components/movieList/index.jsx
+++ src/components/movieList/index.jsx
@@ -1,25 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import movieApi from "apis/movieApi";
+import PageLoader from "components/common/PageLoader";
 import { Search } from "neetoicons";
-import { Input } from "neetoui";
+import { Input, NoData } from "neetoui";
+import { isEmpty } from "ramda";
+import { useTranslation } from "react-i18next";
 
-import { MOVIE_LIST } from "./constants";
 import MovieListItem from "./MovieListItem";
 
-const MovieList = () => (
-  <section className="bg-[#f5f5f5] px-16 py-8">
-    <Input
-      className="outline-none  focus:border-[#add]  focus:ring-1 "
-      placeholder="Search Movies..."
-      prefix={<Search />}
-      type="search"
-    />
-    <div className="mt-8 grid  grid-cols-2 justify-items-center  gap-y-8  md:grid-cols-3 lg:grid-cols-4">
-      {MOVIE_LIST.map(movie => (
-        <MovieListItem key={movie.imdbID} {...movie} />
-      ))}
-    </div>
-  </section>
-);
+const MovieList = () => {
+  const { t } = useTranslation();
+
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState("spiderman");
+
+  const fetchData = async searchQuery => {
+    setLoading(true);
+    try {
+      const { Search: data } = await movieApi.fetch({ s: searchQuery });
+
+      setMovies(data || []);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+      setMovies([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(query);
+  }, [query]);
+
+  if (loading) return <PageLoader />;
+
+  return (
+    <section className="bg-[#f5f5f5] px-16 py-8">
+      <Input
+        className="outline-none focus:border-[#add] focus:ring-1"
+        placeholder={t("searchMovie")}
+        prefix={<Search />}
+        type="search"
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+      />
+      {!isEmpty(movies) ? (
+        <div className="mt-8 grid grid-cols-2 justify-items-center gap-y-8 md:grid-cols-3 lg:grid-cols-4">
+          {movies.map(movie => (
+            <MovieListItem key={movie.imdbID} {...movie} />
+          ))}
+        </div>
+      ) : (
+        <NoData
+          className="flex h-screen w-full items-center justify-center"
+          title={t("noData")}
+        />
+      )}
+    </section>
+  );
+};
 
 export default MovieList;
