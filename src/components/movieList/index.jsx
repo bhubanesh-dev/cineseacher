7cff3cfa9581d650471fcef347ef711cd80cdd59
bhubanesh-dev <bhubanesh.dev@gmail.com>
2025-03-20 20:20:39 +0530

    Implemented debounce search

--- src/components/movieList/index.jsx
+++ src/components/movieList/index.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import movieApi from "apis/movieApi";
 import PageLoader from "components/common/PageLoader";
+import useDebounce from "hooks/useDebounce";
 import { Search } from "neetoicons";
 import { Input, NoData } from "neetoui";
 import { isEmpty } from "ramda";
@@ -11,42 +12,47 @@ import MovieListItem from "./MovieListItem";
 
 const MovieList = () => {
   const { t } = useTranslation();
-
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [query, setQuery] = useState("spiderman");
-
-  const fetchData = async searchQuery => {
-    setLoading(true);
-    try {
-      const { Search: data } = await movieApi.fetch({ s: searchQuery });
-
-      setMovies(data || []);
-    } catch (error) {
-      console.error("Error fetching movies:", error);
-      setMovies([]);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const [query, setQuery] = useState("");
+  const debounceSearchKey = useDebounce(query);
 
   useEffect(() => {
-    fetchData(query);
-  }, [query]);
+    const fetchData = async () => {
+      if (!debounceSearchKey) {
+        setMovies([]);
 
-  if (loading) return <PageLoader />;
+        return;
+      }
+
+      setLoading(true);
+      try {
+        const { Search: data } = await movieApi.fetch({ s: debounceSearchKey });
+        setMovies(data || []);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+        setMovies([]);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [debounceSearchKey]);
 
   return (
-    <section className="bg-[#f5f5f5] px-16 py-8">
+    <section className="flex flex-col bg-[#f5f5f5] px-16 py-8">
       <Input
-        className="outline-none focus:border-[#add] focus:ring-1"
+        className="outline-none my-4 focus:border-[#add] focus:ring-1"
         placeholder={t("searchMovie")}
         prefix={<Search />}
         type="search"
         value={query}
         onChange={e => setQuery(e.target.value)}
       />
-      {!isEmpty(movies) ? (
+      {loading ? (
+        <PageLoader />
+      ) : !isEmpty(movies) ? (
         <div className="mt-8 grid grid-cols-2 justify-items-center gap-y-8 md:grid-cols-3 lg:grid-cols-4">
           {movies.map(movie => (
             <MovieListItem key={movie.imdbID} {...movie} />
