f9bafd9c11452e4f04673aaf3b290b43bd785b3f
bhubanesh-dev <bhubanesh.dev@gmail.com>
2025-03-26 17:33:41 +0530

    Implemented  validation and filter for movies

---
+++ src/components/Movie/MovieList/Filter/TypeCheckBoxes.jsx
@@ -0,0 +1,23 @@
+import { Label, Checkbox } from "neetoui";
+
+const TypeCheckboxes = ({ formik, handleTypeChange, t }) => (
+  <>
+    <Label className="my-4">{t("filterParameters.type")}</Label>
+    <div className="flex flex-row items-center justify-between">
+      <Checkbox
+        checked={formik.values.types.movie}
+        id="checkbox_movie"
+        label={t("filterParameters.movie")}
+        onChange={() => handleTypeChange("movie")}
+      />
+      <Checkbox
+        checked={formik.values.types.series}
+        id="checkbox_series"
+        label={t("filterParameters.series")}
+        onChange={() => handleTypeChange("series")}
+      />
+    </div>
+  </>
+);
+
+export default TypeCheckboxes;
---
+++ src/components/Movie/MovieList/utils/validation.js
@@ -0,0 +1,21 @@
+import i18n from "src/common/i18n";
+import * as Yup from "yup";
+
+const maxYear = new Date().getFullYear() + 5;
+
+export const VALIDATION_SCHEMA = Yup.object().shape({
+  year: Yup.string()
+    .matches(/^\d{4}$/, i18n.t("filterParameters.invalidYear"))
+    .test(
+      "valid-year-range",
+      i18n.t("filterParameters.invalidYear", { min: 2000, max: maxYear }),
+      value => {
+        if (!value) return false;
+        const year = parseInt(value, 10);
+
+        return year >= 2000 && year <= maxYear;
+      }
+    ),
+  movie: Yup.boolean(),
+  series: Yup.boolean(),
+});
