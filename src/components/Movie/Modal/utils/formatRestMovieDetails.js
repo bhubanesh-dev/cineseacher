06d31d08767517fa47cd4e3538497fa424e6931d
bhubanesh-dev <bhubanesh.dev@gmail.com>
2025-03-28 19:54:17 +0530

    Fixed alert appear on empty movie view history

--- src/components/Movie/Modal/utils/formatRestMovieDetails.js
+++ src/components/Movie/Modal/utils/formatRestMovieDetails.js
@@ -1,11 +1,3 @@
-/**
- * Receives a movie object and formats it into an array of { label, value } objects.
- * - `label` contains the localized text using i18next.
- * - `value` contains the corresponding movie detail, converting objects to JSON strings if necessary.
- * - If a value is missing, it falls back to a default translation.
- *
- */
-
 import { t } from "i18next";
 
 export const formatMovieDetails = movie => {
